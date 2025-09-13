e9468b7282b532030556b52112a2c43fe9cfa29a
Batraz2004 <batraz.tolasovv@gmail.com>
2025-09-13 19:13:41 +0300

    instalation roles and permissions

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class Seller extends Authenticatable implements FilamentUser
 {   
+    use HasFactory, Notifiable, HasRoles;
+
     protected $fillable = [
         'name',
         'email',
