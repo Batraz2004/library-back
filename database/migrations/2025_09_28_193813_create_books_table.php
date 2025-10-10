82fcf93925b91195c5acee54e3011778cd9dc5cb
Batraz2004 <batraz.tolasovv@gmail.com>
2025-10-10 23:56:09 +0300

    create migrations and models for library-structure

---
+++ database/migrations/2025_09_28_193813_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained('sellers');
+            $table->string('author');
+            $table->string('isbn')->unique();
+            $table->decimal('price')->default(0.0);
+            $table->integer('count')->default(1);
+            $table->integer('sort_index')->default(500);
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2025_09_28_201428_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('status')->default('active');
+            $table->string('address');
+            $table->integer('sort_index')->default(500);
+            $table->boolean('is_active')->default(true);
+            $table->decimal('full_price',10,3);
+            $table->string('phone');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
