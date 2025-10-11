01d97ed9fa475a3f3df6f1446454c7049fe163aa
Batraz2004 <batraz.tolasovv@gmail.com>
2025-10-11 22:22:25 +0300

    add seder and faker for books

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Seller;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'seller_id' => Seller::query()->first(),
+            'author' => fake()->name(),
+            'isbn' => fake()->uuid(),
+            'price' => fake()->randomFloat(3,250,1200),
+            'count' => rand(1,10),
+            'created_at' => now(),
+        ];
+    }
+}
