bd6d45054bc96862365d326798a1ff2c63bcb91a
Batraz2004 <batraz.tolasovv@gmail.com>
2025-10-11 23:35:10 +0300

    logout

--- app/Http/Controllers/Client/LoginController.php
+++ app/Http/Controllers/Client/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -18,62 +19,43 @@ class LoginController extends Controller
 
         $user = User::query()->firstWhere('email', $request->email);
 
-        if (blank($user) || !Hash::check($request->password, $user->password)) {//хэш  потмоу что user -> password закэширован
+        if (blank($user) || !Hash::check($request->password, $user->password)) { //хэш  потмоу что user -> password закэширован
             return response()->json(['message' => 'не верный логин или пароль'], 200);
         }
 
-        $result = DB::transaction(function() use($user, $tokenName){
+        $result = DB::transaction(function () use ($user, $tokenName) {
             //старые токены удалим
             $query = $user->tokens();
             $query->where('name', $tokenName);
             $query->delete();
-            
+
             $token = $user->createToken($tokenName)->plainTextToken;
-            
+
             $user->remember_token = Hash::make($token);
             $user->save();
 
             return $token;
         });
 
-        if($result)
+        if ($result)
             return response()->json(['message' => 'пользователь авторизован', 'token' => $result], 200);
         else
             return response()->json(['message' => 'не удалост авторизоваться', 'token' => $result], 200);
-
     }
 
-    // public function login(Request $request)
-    // {
-    //     $tokenName = $request->header('user-agent');
-    //     if (filled($request->email) && filled($request->password)) {
-            
-    //         $validateData = $request->validate([
-    //             'email'    => ['required', 'max:255',],
-    //             'password' => ['required', 'max:255'],
-    //         ]);
+    public function logout(Request $request)
+    {
+        //способ для сессии (тогда надо будет убрать мидлвар санктум с роута logout)
+        // Auth::logout();
 
-    //         $user = User::query()->firstWhere('email', $request->email);
+        // $request->session()->invalidate();
 
-    //         if (blank($user) || !Hash::check($request->password, $user->password)) {
-    //             throw ValidationException::withMessages(['pass' => 'Неверный логин или пароль.']);
-    //         }
+        // $request->session()->regenerateToken();
 
-    //         //старые токены удалим
-    //         $query = $user->tokens();
-    //         $query->where('name', $tokenName);
-    //         $query->delete();
+        //способ для апи:
 
-    //         $token = $user->createToken($tokenName)->plainTextToken;
-            
-    //         $user->remember_token = Hash::make($token);
-    //         $user->save();
-    //     }
-    //     else
-    //     {
-    //         return response()->json(['message' => 'пользователь уже существует'], 200);
-    //     }
+        $request->user()->currentAccessToken()->delete();
 
-    //     return response()->json(['message' => 'пользователь успешно создан', 'token' => $token], 200);
-    // }
+        return response()->json(['code' => 200, 'mess' => 'выход из аккаунта'], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,26 +3,39 @@
 use App\Http\Controllers\Client\LoginController;
 use App\Http\Controllers\Client\RegistrationController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/test', function (Request $req) {
     return response()->json([
         'code' => 200,
-        'message' => 'its work',
+        'message' => 'You authorize!',
         'req_contebt' => $req->getContent()
     ]);
 });
 Route::get('/authTest',function(){
     return response()->json([
         'code' => 200,
-         'message' => 'its work',
+         'message' => 'You authorize!',
     ]);
 })->middleware('auth:sanctum');
 
 // Route::post('user',);
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sunctum');
+})->middleware('auth:sanctum');
 
 Route::post('registration', [RegistrationController::class, 'createUser']);
 Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+
+
+Route::prefix('bookmarks')->group(function(){
+    Route::post('add',function(Request $request){
+        if(!Auth::check())
+        {
+
+        }
+    });
+});
+
