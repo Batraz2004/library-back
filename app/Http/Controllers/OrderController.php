4b6ff30aa04790ba321a8ad1f786ef7288714fd8
Batraz2004 <batraz.tolasovv@gmail.com>
2025-11-04 18:38:59 +0300

    add scopes for active accounts

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CashAccount;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,6 +17,5 @@ class OrderController extends Controller
         $query = $user->cart->cartItems()->getQuery();
         $checkedCartItems = $query->isActive()->isChecked()->get();
 
-        
     }
 }
--- app/Models/CashAccount.php
+++ app/Models/CashAccount.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\AccountActiveScope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -11,6 +13,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property string $currency
  * @property string $status
  */
+
+#[ScopedBy([AccountActiveScope::class])]
 class CashAccount extends Model
 {
     protected $guarded = [];
